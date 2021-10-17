3b75713df31e58d39da1faf80835c958cb04e676
Juliano Teodoro <julianoteodoro2002@gmail.com>
2021-10-17 17:58:29 -0300

    ExercicioJS1

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ const remove = alunos.pop();
 console.log(remove);
 console.log(removido);
 console.log(alunos);
+
+function criaPessoa(nome, sobrenome, idade){
+    const pessoa = {
+        nome,
+        sobrenome,
+        idade,
+    
+        fala(){
+            console.log("Ola mundo");
+        },
+         
+       incrementaIdade(){
+        this.idade++;
+       }
+
+    };
+    return pessoa;
+}
+
+const pessoa1 = criaPessoa("Juliano", "Teodoro", 18);
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);
+console.log(pessoa1.idade);
+
+pessoa1.fala();
